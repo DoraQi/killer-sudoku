8afe7ecb5c1588e62bdc5859c5d998bd84b9cec7
RN <rustamch121@gmail.com>
2024-04-03 15:41:37 -0700

    serve ping

--- server.pl
+++ server.pl
@@ -5,15 +5,37 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_files)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_json)).
 
 % Load web app modules.
 :- [app/init].
 
 % Serve static files for each web app module.
-:- http_handler('/static', serve_static, [prefix]).
+:- http_handler('/api/ping', serve_ping, []).
+% :- http_handler('/api/sudoku/new-puzzle', serve_new_sudoku, []).
+% :- http_handler('/api/sudoku/verify-puzzle', serve_verify_soln, []).
 
-serve_static(Request) :-
-    http_reply_from_files('app/static/app', [], Request).
+
+serve_ping(Request) :-
+    reply_json_dict(_{status: "ok"}).
+
+reply_json_dict(Dict) :-
+    reply_json(Dict, [json_object(dict)]).
+
+% Serve the new sudoku puzzle.
+
+% serve_new_sudoku(Request) :-
+%     new_sudoku_puzzle(Puzzle),
+%     reply_json_dict(Puzzle).
+
+
+% % Serve the solution verification.
+% serve_verify_soln(Request) :-
+%     http_read_json_dict(Request, JSON),
+%     verify_sudoku_soln(JSON, Result),
+%     reply_json_dict(Result).
+
+
 
 serve_static(Request) :-
     http_404([], Request).
