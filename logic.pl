79757fe51f5b45a6daefbd8783ac864f8f07eebc
doraqi <dora.qy86@gmail.com>
2024-04-08 14:45:36 -0700

    add more killer solver logic, encountering argument not sufficiently instantiated error

--- logic.pl
+++ logic.pl
@@ -53,26 +53,28 @@ rand_fill(X) :-
 %   - a 9x9 2D matrix of labels representing the cage each square is in
 %   - an array where i-th element is the sum of cage i
 
-killer_sudoku(Rows, Cages) :-
+killer_sudoku(Rows, Cages, CageValues) :-
     sudoku(Rows),
-    obey_cages(Rows, Cages).
+    obey_cages(Rows, Cages, CageValues).
 
-obey_cages(Rows, Cages) :-
-    sum_cages(Rows, Cages, Sums),
-    equal(Sums, Cages).
+obey_cages(Rows, Cages, CageValues) :-
+    generate_zero_list(81, Zeros),
+    sum_cages(Rows, Cages, Zeros, Sums),
+    equal(Sums, CageValues).
 
 sum_cages([], [], Sums, Sums).
 sum_cages([Row|Rest], [CRow|CRest], Sums, Sums2) :-
     sum_row(Row, CRow, Sums, Sums1),
     sum_cages(Rest, CRest, Sums1, Sums2).
 
-sum_row([], _, Sum, Sum).
+sum_row([X], [XCage], Cage_sums, Cage_sums2) :-
+    update_ith(XCage, Cage_sums, X, Cage_sums2).
 sum_row([X|T], [XCage|TCages], Cage_sums, Cage_sums2) :-
     update_ith(XCage, Cage_sums, X, Cage_sums1),
     sum_row(T, TCages, Cage_sums1, Cage_sums2).
 
 update_ith(0, [H|T], X, [X1|T]) :-
-    X1 is X + H.
+    X1 #= X + H.
 update_ith(I, [H|T], X, [H|T2]) :-
     I1 is I-1,
     update_ith(I1, T, X, T2).
@@ -82,14 +84,14 @@ equal([H1|T1], [H2|T2]) :-
     H1 = H2,
     equal(T1, T2).
 
-% generate_zero_list(0, []).
-% generate_zero_list(N, [0|Zeros]) :-
-%     N > 0,                 
-%     N1 is N - 1,          
-%     generate_zero_list(N1, Zeros). 
-% add_elem(X, Y) :-
-%     random(1, 10, Z),
-%     append(X, Z, Y).
+generate_zero_list(0, []).
+generate_zero_list(N, [0|Zeros]) :-
+    N > 0,                 
+    N1 is N - 1,          
+    generate_zero_list(N1, Zeros). 
+add_elem(X, Y) :-
+    random(1, 10, Z),
+    append(X, Z, Y).
 
 
 % % complete_board(Before, After) :-
