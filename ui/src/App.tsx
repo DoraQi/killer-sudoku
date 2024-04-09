346eaa918172340b92bf30e2c41a02359de8ff5b
RN <rustamch121@gmail.com>
2024-04-09 14:05:17 -0700

    more css hell

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -5,45 +5,6 @@ import Button from './components/Button';
 
 const initialBoard = Array(9).fill(null).map(() => Array(9).fill({ value: '', cageId: null }));
 
-function checkSolution(board: { value: string, cageId: number }[][], cages: { cageId: number, cells: number[][], sum: number }[]) {
-  // copy values in board to a 2D array of integers
-  const boardValues = board.map(row => row.map(cell => parseInt(cell.value) || 0));
-  // copy cageId in board to a 2D array of integers
-  const boardCageIds = board.map(row => row.map(cell => cell.cageId));
-  // print boardValues and boardCageIds
-  console.log("boardValue=", boardValues);
-  console.log("boardCageIds=", boardCageIds);
-  // store cages sorted by cageId
-  const cagesById = cages.slice().sort((a, b) => a.cageId - b.cageId);
-  // store the sum of each cage into an array
-  const cageSums = cagesById.map(cage => cage.sum);
-
-  // call http://localhost:8000/api/killer-sudoku/verify-solution with the boardValues and boardCageIds and cageSums
-  fetch('http://localhost:8000/api/killer-sudoku/verify-puzzle', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      board: boardValues,
-      cages: boardCageIds,
-      cagevalues: cageSums
-    })
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.valid) {
-        alert('Solution is correct!');
-      } else {
-        alert('Solution is incorrect!');
-      }
-    });
-}
 
 function App() {
   const [board, setBoard] = useState(initialBoard);
@@ -86,7 +47,7 @@ function App() {
           <div className={"buttonRow"}>
             <Button buttonText="Reset Entries" onClick={() => { setBoard(initialBoard) }} />
             <Button buttonText="Generate New Board" onClick={() => { setReload(!reload) }} />
-            <Button buttonText="Check Solution" onClick={() => { checkSolution(board, cages) }} />
+            <Button buttonText="Check Solution" onClick={() => { alert('Checking solution') }} />
           </div>
           <KillerSudoku
             board={board}
