f3cc22f3511fb5989a9865bd3b8bccf19407ed76
RN <rustamch121@gmail.com>
2024-04-09 14:05:50 -0700

    less css the better

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -33,17 +33,7 @@ function App() {
       {loading ? (
         <div className="loading-wheel">Loading...</div>
       ) : (
-        <div style={
-          {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-            padding: '4rem'
-          }
-
-        }>
+        <div>
           <div className={"buttonRow"}>
             <Button buttonText="Reset Entries" onClick={() => { setBoard(initialBoard) }} />
             <Button buttonText="Generate New Board" onClick={() => { setReload(!reload) }} />
